9be6441c945d9dfc197e09be1490abda462e43ad
arpitasarang98 <81257878+arpitasarang98@users.noreply.github.com>
2021-03-25 01:59:07 +0530

    Add files via upload

---
+++ Prac6_PerformBlastAndNoOfRepition.py
@@ -0,0 +1,24 @@
+# program to find count of repetition of a sequence for a given nucleotide.
+file=open("genes.txt","r")  
+r=file.read()  
+size=len(r)  
+score_A=0  
+score_C=0  
+score_T=0  
+score_G=0  
+for i in range(size):  
+     if(r[i]=='A'):  
+          score_A+=1  
+     elif (r[i]=='C'): 
+          score_C+=1  
+     elif (r[i]=='T'): 
+          score_T+=1  
+     elif (r[i]=='G'): 
+          score_G+=1  
+print("score of A is ",score_A)  
+print("score of C is ",score_C)  
+print("score of T is ",score_T)  
+print("score of G is ",score_G)
+
+""" Genes.txt should be in the same folder/location as python file for count
+of repetition of sequence. """
